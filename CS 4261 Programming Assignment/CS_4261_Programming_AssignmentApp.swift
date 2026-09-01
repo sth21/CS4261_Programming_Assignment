9ece4a49e792de1523f0e437238e8aeb60925dfd
Sam Heseltine <bills2021e@gmail.com>
2026-08-31 20:17:27 -0400

    Initial Commit

---
+++ CS 4261 Programming Assignment/CS_4261_Programming_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS_4261_Programming_AssignmentApp.swift
+//  CS 4261 Programming Assignment
+//
+//  Created by Sam Heseltine on 8/31/26.
+//
+
+import SwiftUI
+
+@main
+struct CS_4261_Programming_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS 4261 Programming Assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CS 4261 Programming Assignment
+//
+//  Created by Sam Heseltine on 8/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
