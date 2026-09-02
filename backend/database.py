044709ad591f988972d8d19a348f133ca9d4d1f1
Sam Heseltine <bills2021e@gmail.com>
2026-09-02 13:12:17 -0400

    Test both remotes work

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./gameday.db")
 
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 
-engine = create_engine(DATABASE_URL, echo=True, connect_args=connect_args)
+engine = create_engine(DATABASE_URL, echo=False, connect_args=connect_args)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
